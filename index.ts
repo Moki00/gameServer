00780cda757e1cb567edf390a8f83bdbab6c6f1b
Moki <moking929@gmail.com>
2022-10-22 11:21:25 -0400

    convert to ts

--- index.ts
+++ index.ts
@@ -11,8 +11,8 @@ app.use((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is a test server</h1></body></html>');
- 
 });
+
 const sample_server = http.createServer(app);
  
 sample_server.listen(port, hostname, () => {
