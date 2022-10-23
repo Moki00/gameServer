d12982db624c3fed6f2726d719f6ba8f248c5587
Moki <moking929@gmail.com>
2022-10-22 23:07:58 -0400

    const

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express from "express";
+const express = require('express')
+
 import { connectToDatabase } from "./services/database.service"
 import { gamesRouter } from "./routes/games.router";
 
