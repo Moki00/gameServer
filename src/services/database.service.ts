7408e244823fa94a0750e809816e7d912d4ff74e
Moki <moking929@gmail.com>
2022-10-23 14:51:43 -0400

    bson or json

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -6,18 +6,44 @@ import * as dotenv from "dotenv";
 export const collections: { games?: mongoDB.Collection } = {}
 
 // Initialize Connection
-export async function connectToDatabase () {
-    dotenv.config();
- 
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
-            
-    await client.connect();
-        
-    const db: mongoDB.Db = client.db(process.env.DB_NAME);
-   
-    const gamesCollection: mongoDB.Collection = db.collection(process.env.GAMES_COLLECTION_NAME);
- 
+export async function connectToDatabase() {
+  dotenv.config();
+
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
+
+  await client.connect();
+
+  const db: mongoDB.Db = client.db(process.env.DB_NAME);
+
+  await db.command({
+    "collMod": process.env.GAMES_COLLECTION_NAME,
+    "validator": {
+      $jsonSchema: {
+        bsonType: "object",
+        required: ["name", "price", "category"],
+        additionalProperties: false,
+        properties: {
+          _id: {},
+          name: {
+            bsonType: "string",
+            description: "'name' is required and is a string"
+          },
+          price: {
+            bsonType: "number",
+            description: "'price' is required and is a number"
+          },
+          category: {
+            bsonType: "string",
+            description: "'category' is required and is a string"
+          }
+        }
+      }
+    }
+  });
+
+  const gamesCollection: mongoDB.Collection = db.collection(process.env.GAMES_COLLECTION_NAME);
+
   collections.games = gamesCollection;
-       
-         console.log(`Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`);
- }
+
+  console.log(`Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`);
+}
